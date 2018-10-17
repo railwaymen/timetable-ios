02ae70d962ae7520d0f54d9fa1b1f736596dad45
Piotr Pawlus <pawlus.piotrek@gmail.com>
2018-10-17 10:34:07 +0200

    Created clear iOS xcode project Added gitignore file

---
+++ TimeTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTable
+//
+//  Created by Piotr Pawluś on 17/10/2018.
+//  Copyright © 2018 Railwaymen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TimeTableTests/TimeTableTests.swift
@@ -0,0 +1,34 @@
+//
+//  TimeTableTests.swift
+//  TimeTableTests
+//
+//  Created by Piotr Pawluś on 17/10/2018.
+//  Copyright © 2018 Railwaymen. All rights reserved.
+//
+
+import XCTest
+@testable import TimeTable
+
+class TimeTableTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
