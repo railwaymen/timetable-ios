f5ccedbdd5416eea7dada66c315420f5a3edbc55
Piotr Pawlus <pawlus.piotrek@gmail.com>
2018-10-25 09:08:01 +0200

    Created base class for app flow coordinators Tests - extend String with Error protocol Tests - unwrap method for Optional

---
+++ TimeTable/Coordinators/BaseCoordinator.swift
@@ -0,0 +1,43 @@
+//
+//  BaseCoordinator.swift
+//  TimeTable
+//
+//  Created by Piotr Pawluś on 25/10/2018.
+//  Copyright © 2018 Railwaymen. All rights reserved.
+//
+
+import UIKit
+
+class Coordinator: CoordinatorType {
+    
+    var identifier: UUID = UUID()
+    var children: [Coordinator: Any]
+    var window: UIWindow?
+    var finishCompletion: (() -> Void)?
+    
+    //MARK: - Initialization
+    
+    init(window: UIWindow?) {
+        self.identifier = UUID()
+        self.children = [:]
+        self.window = window
+    }
+    
+    //MARK: - CoordinatorType
+    
+    func start(finishCompletion: (() -> Void)?) {
+        self.finishCompletion = finishCompletion
+    }
+    
+    func finish() {
+        finishCompletion?()
+    }
+    
+    func addChildCoordinator(child: Coordinator) {
+        children[child] = child
+    }
+    
+    func removeChildCoordinator(child: Coordinator) {
+        children[child] = nil
+    }
+}
---
+++ TimeTableTests/Common/Error+String.swift
@@ -0,0 +1,11 @@
+//
+//  Error+String.swift
+//  TimeTableTests
+//
+//  Created by Piotr Pawluś on 25/10/2018.
+//  Copyright © 2018 Railwaymen. All rights reserved.
+//
+
+import Foundation
+
+extension String: Error {}
