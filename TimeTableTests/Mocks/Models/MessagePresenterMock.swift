b3b220d52a045b21554214a53915f151489445a4
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-02-20 12:08:50 +0100

    Added confirmation pop-up on entry delete action

--- TimeTableTests/Mocks/Models/MessagePresenterMock.swift
+++ TimeTableTests/Mocks/Models/MessagePresenterMock.swift
@@ -14,11 +14,27 @@ class MessagePresenterMock {
     struct PresentAlertControllerParams {
         var message: String
     }
+    
+    private(set) var requestDecisionParams: [RequestDecisionParams] = []
+    struct RequestDecisionParams {
+        let title: String?
+        let message: String?
+        let cancelButtonConfig: ButtonConfig
+        let confirmButtonConfig: ButtonConfig
+    }
 }
 
 // MARK: - MessagePresenterType
 extension MessagePresenterMock: MessagePresenterType {
     func presentAlertController(withMessage message: String) {
         self.presentAlertControllerParams.append(PresentAlertControllerParams(message: message))
+    }
+    
+    func requestDecision(title: String?, message: String?, cancelButtonConfig: ButtonConfig, confirmButtonConfig: ButtonConfig) {
+        self.requestDecisionParams.append(RequestDecisionParams(
+            title: title,
+            message: message,
+            cancelButtonConfig: cancelButtonConfig,
+            confirmButtonConfig: confirmButtonConfig))
     }
 }
