ac7e8c10bdab90622f8d6eb34c715896dbff3ee3
Piotr Pawluś <piotr.pawlus@railwaymen.org>
2020-04-30 12:31:53 +0200

    Update NewVacationViewControllerMock

--- TimeTableTests/Mocks/Categories/NewVacation/NewVacationViewControllerMock.swift
+++ TimeTableTests/Mocks/Categories/NewVacation/NewVacationViewControllerMock.swift
@@ -12,6 +12,53 @@ import XCTest
 class NewVacationViewControllerMock: UIViewController {
     
     // MARK: - NewVacationViewModelOutput
+    private(set) var setUpParams: [SetUpParams] = []
+    struct SetUpParams {}
+    
+    private(set) var setActivityIndicatorParams: [SetActivityIndicatorParams] = []
+    struct SetActivityIndicatorParams {
+        let isHidden: Bool
+    }
+    
+    private(set) var setNoteParams: [SetNoteParams] = []
+    struct SetNoteParams {
+        let text: String
+    }
+    
+    private(set) var setMinimumDateForEndDateParams: [SetMinimumDateForEndDateParams] = []
+    struct SetMinimumDateForEndDateParams {
+        let minDate: Date
+    }
+    
+    private(set) var updateStartDateParams: [UpdateStartDateParams] = []
+    struct UpdateStartDateParams {
+        let date: Date
+        let dateString: String
+    }
+    
+    private(set) var updateEndAtDateParmas: [UpdateEndAtDateParmas] = []
+    struct UpdateEndAtDateParmas {
+        let date: Date
+        let dateString: String
+    }
+    
+    private(set) var updateTypeParams: [UpdateTypeParams] = []
+    struct UpdateTypeParams {
+        let name: String
+    }
+    
+    private(set) var setSaveButtonParams: [SetSaveButtonParams] = []
+    struct SetSaveButtonParams {
+        let isEnabled: Bool
+    }
+    
+    private(set) var setBottomContentInsetParams: [SetBottomContentInsetParams] = []
+    struct SetBottomContentInsetParams {
+        let height: CGFloat
+    }
+    
+    private(set) var dismissKeyboardParams: [DismissKeyboardParams] = []
+    struct DismissKeyboardParams {}
    
     // MARK: - NewVacationViewControllerType
     private(set) var configureParams: [ConfigureParams] = []
@@ -21,7 +68,47 @@ class NewVacationViewControllerMock: UIViewController {
 }
 
 // MARK: - NewVacationViewModelOutput
-extension NewVacationViewControllerMock: NewVacationViewModelOutput {}
+extension NewVacationViewControllerMock: NewVacationViewModelOutput {
+    func setUp() {
+        self.setUpParams.append(SetUpParams())
+    }
+    
+    func setActivityIndicator(isHidden: Bool) {
+        self.setActivityIndicatorParams.append(SetActivityIndicatorParams(isHidden: isHidden))
+    }
+    
+    func setNote(text: String) {
+        self.setNoteParams.append(SetNoteParams(text: text))
+    }
+    
+    func setMinimumDateForEndDate(minDate: Date) {
+        self.setMinimumDateForEndDateParams.append(SetMinimumDateForEndDateParams(minDate: minDate))
+    }
+    
+    func updateStartDate(with date: Date, dateString: String) {
+        self.updateStartDateParams.append(UpdateStartDateParams(date: date, dateString: dateString))
+    }
+    
+    func updateEndAtDate(with date: Date, dateString: String) {
+        self.updateEndAtDateParmas.append(UpdateEndAtDateParmas(date: date, dateString: dateString))
+    }
+    
+    func updateType(name: String) {
+        self.updateTypeParams.append(UpdateTypeParams(name: name))
+    }
+
+    func setSaveButton(isEnabled: Bool) {
+        self.setSaveButtonParams.append(SetSaveButtonParams(isEnabled: isEnabled))
+    }
+    
+    func setBottomContentInset(_ height: CGFloat) {
+        self.setBottomContentInsetParams.append(SetBottomContentInsetParams(height: height))
+    }
+    
+    func dismissKeyboard() {
+        self.dismissKeyboardParams.append(DismissKeyboardParams())
+    }
+}
 
 // MARK: - NewVacationViewControllerType
 extension NewVacationViewControllerMock: NewVacationViewControllerType {
