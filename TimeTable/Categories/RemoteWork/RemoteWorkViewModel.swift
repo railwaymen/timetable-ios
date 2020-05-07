41d444bba10f2a13728ce5766606f835a335281e
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-05-07 12:37:32 +0200

    Added function for fetching next page

--- TimeTable/Categories/RemoteWork/RemoteWorkViewModel.swift
+++ TimeTable/Categories/RemoteWork/RemoteWorkViewModel.swift
@@ -30,7 +30,8 @@ class RemoteWorkViewModel {
     
     private let apiClient: ApiClientRemoteWorkType
     private let itemsPerPage = 24
-    private var response: RemoteWorkResponse?
+    private var state: State?
+    private var records: [RemoteWork] = []
     
     // MARK: - Initialization
     init(
@@ -44,6 +45,15 @@ class RemoteWorkViewModel {
     }
 }
 
+// MARK: - Structures
+extension RemoteWorkViewModel {
+    enum State: Equatable {
+        case fetching(page: Int)
+        case fetched(page: Int, totalPages: Int)
+        case firstPageFetchFailed
+    }
+}
+
 // MARK: - RemoteWorkViewModelType
 extension RemoteWorkViewModel: RemoteWorkViewModelType {
     func loadView() {
@@ -51,7 +61,7 @@ extension RemoteWorkViewModel: RemoteWorkViewModelType {
     }
     
     func viewWillAppear() {
-        self.fetchRemoteWorks()
+        self.fetchFirstPage()
     }
     
     func addNewRecordTapped() {
@@ -63,7 +73,7 @@ extension RemoteWorkViewModel: RemoteWorkViewModelType {
     }
     
     func numberOfItems() -> Int {
-        return self.response?.records.count ?? 0
+        return self.records.count
     }
     
     func configure(_ cell: RemoteWorkCellable, for indexPath: IndexPath) {
@@ -76,31 +86,67 @@ extension RemoteWorkViewModel: RemoteWorkViewModelType {
 // MARK: - Private
 extension RemoteWorkViewModel {
     private func item(for index: IndexPath) -> RemoteWork? {
-        return self.response?.records[safeIndex: index.row]
+        self.records[safeIndex: index.row]
     }
     
-    private func fetchRemoteWorks() {
-        // TODO - TIM-293 pagination
-        let parameters = RemoteWorkParameters(page: 0, perPage: self.itemsPerPage)
+    private func fetchFirstPage() {
+        guard self.state == .none || self.state == .firstPageFetchFailed else { return }
+        let pageNumber = 1
+        let parameters = self.parameters(forPage: pageNumber)
+        self.state = .fetching(page: pageNumber)
         self.userInterface?.setActivityIndicator(isHidden: false)
         _ = self.apiClient.fetchRemoteWork(parameters: parameters) { [weak self] result in
             self?.userInterface?.setActivityIndicator(isHidden: true)
             switch result {
             case let .success(response):
-                self?.handleFetch(response: response)
+                self?.handleFirstPageFetchSuccess(response: response)
             case let .failure(error):
-                self?.handleFetch(error: error)
+                self?.handleFirstPageFetchFailure(error: error)
             }
         }
     }
     
-    private func handleFetch(response: RemoteWorkResponse) {
-        self.response = response
+    private func handleFirstPageFetchSuccess(response: RemoteWorkResponse) {
+        self.records = response.records
+        self.state = .fetched(page: 1, totalPages: response.totalPages)
         self.userInterface?.showTableView()
         self.userInterface?.updateView()
     }
     
-    private func handleFetch(error: Error) {
-        // TODO - TIM-292 error handling
+    private func handleFirstPageFetchFailure(error: Error) {
+        self.state = .firstPageFetchFailed
+        // TODO: TIM-292 error handling
+    }
+    
+    private func fetchNextPage() {
+        guard let previousState = self.state else { return }
+        guard case let .fetched(lastFetchedPage, totalPages) = self.state else { return }
+        guard lastFetchedPage < totalPages else { return }
+        let nextPage = lastFetchedPage + 1
+        let parameters = self.parameters(forPage: nextPage)
+        self.state = .fetching(page: nextPage)
+        _ = self.apiClient.fetchRemoteWork(parameters: parameters) { [weak self] result in
+            switch result {
+            case let .success(response):
+                self?.handleNextPageFetchSuccess(response: response, pageFetched: nextPage)
+            case let .failure(error):
+                self?.handleNextPageFetchFailure(error: error, previousState: previousState)
+            }
+        }
+    }
+    
+    private func handleNextPageFetchSuccess(response: RemoteWorkResponse, pageFetched: Int) {
+        self.records.append(contentsOf: response.records)
+        self.state = .fetched(page: pageFetched, totalPages: response.totalPages)
+        self.userInterface?.updateView()
+    }
+    
+    private func handleNextPageFetchFailure(error: Error, previousState: State) {
+        self.state = previousState
+        // TODO: TIM-292 error handling
+    }
+    
+    private func parameters(forPage page: Int) -> RemoteWorkParameters {
+        RemoteWorkParameters(page: page, perPage: self.itemsPerPage)
     }
 }
