516c1303561df53fa4732ff82d055472e529234f
Piotr Pawluś <piotr.pawlus@railwaymen.org>
2020-05-07 10:06:18 +0200

    Adjusted mock for RemoteWorkViewController

--- TimeTableTests/Mocks/Categories/RemoteWork/RemoteWorkViewControllerMock.swift
+++ TimeTableTests/Mocks/Categories/RemoteWork/RemoteWorkViewControllerMock.swift
@@ -15,6 +15,17 @@ class RemoteWorkViewControllerMock: UIViewController {
     private(set) var setUpParams: [SetUpParams] = []
     struct SetUpParams {}
     
+    private(set) var showTableViewParams: [ShowTableViewParams] = []
+    struct ShowTableViewParams {}
+    
+    private(set) var setActivityIndicatorParams: [SetActivityIndicatorParams] = []
+    struct SetActivityIndicatorParams {
+        let isHidden: Bool
+    }
+
+    private(set) var updateViewParams: [UpdateViewParams] = []
+    struct UpdateViewParams {}
+    
     // MARK: - RemoteWorkViewControllerType
     private(set) var configureParams: [ConfigureParams] = []
     struct ConfigureParams {
@@ -26,6 +37,18 @@ class RemoteWorkViewControllerMock: UIViewController {
 extension RemoteWorkViewControllerMock: RemoteWorkViewModelOutput {
     func setUp() {
         self.setUpParams.append(SetUpParams())
+    }
+    
+    func showTableView() {
+        self.showTableViewParams.append(ShowTableViewParams())
+    }
+    
+    func setActivityIndicator(isHidden: Bool) {
+        self.setActivityIndicatorParams.append(SetActivityIndicatorParams(isHidden: isHidden))
+    }
+    
+    func updateView() {
+        self.updateViewParams.append(UpdateViewParams())
     }
 }
 
