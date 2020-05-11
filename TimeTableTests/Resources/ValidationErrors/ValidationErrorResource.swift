6d43c1f1ac13b7c7bbdefe51b2d7d274d7d30ceb
Piotr Pawluś <piotr.pawlus@railwaymen.org>
2020-05-11 11:28:59 +0200

    Added tests for ValidationError

---
+++ TimeTableTests/Resources/ValidationErrors/ValidationErrorResource.swift
@@ -0,0 +1,14 @@
+//
+//  ValidationErrorResource.swift
+//  TimeTableTests
+//
+//  Created by Piotr Pawluś on 11/05/2020.
+//  Copyright © 2020 Railwaymen. All rights reserved.
+//
+
+import Foundation
+
+enum ValidationErrorResource: String, JSONFileResource {
+    case validationErrorFullModelResponse
+    case validationErrorEmptyValidationErrorResponse
+}
---
+++ TimeTableTests/Tests/Models/Errors/ValidationErrors/ValidationErrorTests.swift
@@ -0,0 +1,91 @@
+//
+//  ValidationErrorTests.swift
+//  TimeTableTests
+//
+//  Created by Piotr Pawluś on 11/05/2020.
+//  Copyright © 2020 Railwaymen. All rights reserved.
+//
+
+import XCTest
+import Restler
+@testable import TimeTable
+
+class ValidationErrorTests: XCTestCase {}
+
+// MARK: - Init Restler.Response
+extension ValidationErrorTests {
+    func testInitializationFromResponse_invalidStatusCode() throws {
+        //Arrange
+        let data = try self.json(from: ValidationErrorResource.validationErrorFullModelResponse)
+        let httpURLResponse = try self.buildHTTPURLResponse(statusCode: 500)
+        let response = Restler.Response(data: data, response: httpURLResponse, error: nil)
+        //Act
+        let sut = ValidationError<RegisterRemoteWorkValidationError>(response: response)
+        //Assert
+        XCTAssertNil(sut)
+    }
+    
+    func testInitializationFromResponse_nilData() throws {
+        //Arrange
+        let httpURLResponse = try self.buildHTTPURLResponse(statusCode: 422)
+        let response = Restler.Response(data: nil, response: httpURLResponse, error: nil)
+        //Act
+        let sut = ValidationError<RegisterRemoteWorkValidationError>(response: response)
+        //Assert
+        XCTAssertNil(sut)
+    }
+    
+    func testInitializationFromResponse_invalidData() throws {
+        //Arrange
+        let httpURLResponse = try self.buildHTTPURLResponse(statusCode: 422)
+        let response = Restler.Response(data: Data(), response: httpURLResponse, error: nil)
+        //Act
+        let sut = ValidationError<RegisterRemoteWorkValidationError>(response: response)
+        //Assert
+        XCTAssertNil(sut)
+    }
+    
+    func testInitializationFromResponse_validationErrorIsEmpty() throws {
+        //Arrange
+        let data = try self.json(from: ValidationErrorResource.validationErrorEmptyValidationErrorResponse)
+        let httpURLResponse = try self.buildHTTPURLResponse(statusCode: 422)
+        let response = Restler.Response(data: data, response: httpURLResponse, error: nil)
+        //Act
+        let sut = ValidationError<RegisterRemoteWorkValidationError>(response: response)
+        //Assert
+        XCTAssertNil(sut)
+    }
+    
+    func testInitializationFromResponse_success() throws {
+        //Arrange
+        let data = try self.json(from: ValidationErrorResource.validationErrorFullModelResponse)
+        let httpURLResponse = try self.buildHTTPURLResponse(statusCode: 422)
+        let response = Restler.Response(data: data, response: httpURLResponse, error: nil)
+        //Act
+        let sut = ValidationError<RegisterRemoteWorkValidationError>(response: response)
+        //Assert
+        XCTAssertEqual(sut?.errors.startsAt.count, 4)
+        XCTAssertEqual(sut?.errors.endsAt.count, 1)
+    }
+}
+
+// MARK: - Decodable
+extension ValidationErrorTests {
+    func testDecoding() throws {
+        //Arrange
+        let data = try self.json(from: ValidationErrorResource.validationErrorFullModelResponse)
+        //Act
+        let sut = try self.decoder.decode(ValidationError<RegisterRemoteWorkValidationError>.self, from: data)
+        //Assert
+        XCTAssertEqual(sut.errors.endsAt.count, 1)
+        XCTAssertEqual(sut.errors.startsAt.count, 4)
+    }
+}
+
+// MARK: - Private
+extension ValidationErrorTests {
+    private func buildHTTPURLResponse(statusCode: Int) throws -> HTTPURLResponse {
+        let url = try XCTUnwrap(URL(string: "https://www.example.com"))
+        return try XCTUnwrap(HTTPURLResponse(url: url, statusCode: statusCode, httpVersion: nil, headerFields: nil))
+    }
+}
